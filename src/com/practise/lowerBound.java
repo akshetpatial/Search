a54d2f130ed1ea4b7b2e8d4e258fb7575d43b289
akshetpatial <akshet.patial@gmail.com>
2023-09-02 14:23:52 +0530

    Added the lowerBound method

--- src/com/practise/lowerBound.java
+++ src/com/practise/lowerBound.java
@@ -5,7 +5,27 @@ import java.util.Scanner;
 public class lowerBound {
 
     public static int lowerBound(int []arr, int n, int x) {
-        
+        int start = 0;
+        int end = n-1;
+        while(start<=end){
+            int mid = end + (start-end)/2;
+//            System.out.println("start"+start);
+//            System.out.println("end"+end);
+//            System.out.println("mid"+mid);
+            if(mid == 0)
+                return mid;
+            // if(mid == end)
+            //     return mid+1;
+            if(start==n-1)
+                return n;
+            if(start==end)
+                return start;
+            if(arr[mid] <x){
+                start = mid+1;
+            }else{
+                end = mid;
+            }
+        }
         return 0;
     }
     public static void main(String[] args) {
@@ -18,8 +38,8 @@ public class lowerBound {
             arr[i]=scan.nextInt();
         System.out.println("Entered array: ");
         for(int ele : arr)
-            System.out.println(ele+" ");
-        System.out.println("Enter the lower bound target");
+            System.out.print(ele+" ");
+        System.out.println("\nEnter the lower bound target");
         int x = scan.nextInt();
         int ans = lowerBound(arr,n,x);
         System.out.println(ans);
